bd56303b53b832289cb468f4e8e0456fae244d06
dmarroquin-2024108 <dmarroquin-2024108@kinal.edu.gt>
2026-05-02 02:04:57 -0600

    feat(user): Agregar las rutas de menu y su interfaz para los usuarios

--- reports-service/src/ReportService.Api/obj/Debug/net8.0/ReportService.Api.AssemblyInfo.cs
+++ reports-service/src/ReportService.Api/obj/Debug/net8.0/ReportService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2443eb7aaa00517b9834c73039b552525bb56094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39e791a8029dc1ca4f3537240d70e2f65dc84a61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
